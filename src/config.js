6a74db510d65fb579111298e173aa5965fc7d675
Noven Purnell-Webb <noven@liberant.com.au>
2017-12-30 02:04:54 +1000

    config

--- src/config.js
+++ src/config.js
@@ -6,20 +6,20 @@ import grants from './grants'
 
 const config = {
   firebase_config: {
-    apiKey: 'AIzaSyBQAmNJ2DbRyw8PqdmNWlePYtMP0hUcjpY',
-    authDomain: 'react-most-wanted-3b1b2.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-3b1b2.firebaseio.com',
-    projectId: 'react-most-wanted-3b1b2',
-    storageBucket: 'react-most-wanted-3b1b2.appspot.com',
-    messagingSenderId: '258373383650'
+    apiKey: "AIzaSyDR6-mGXKNnkJ0Sv7ufX8oNCGYk5UxVVr0",
+    authDomain: "spotaproject.firebaseapp.com",
+    databaseURL: "https://spotaproject.firebaseio.com",
+    projectId: "spotaproject",
+    storageBucket: "spotaproject.appspot.com",
+    messagingSenderId: "746405070068"
   },
   firebase_config_dev: {
-    apiKey: 'AIzaSyB31cMH9nJnERC1WCWA7lQHnY08voLs-Z0',
-    authDomain: 'react-most-wanted-dev.firebaseapp.com',
-    databaseURL: 'https://react-most-wanted-dev.firebaseio.com',
-    projectId: 'react-most-wanted-dev',
-    storageBucket: 'react-most-wanted-dev.appspot.com',
-    messagingSenderId: '70650394824'
+    apiKey: "AIzaSyDR6-mGXKNnkJ0Sv7ufX8oNCGYk5UxVVr0",
+    authDomain: "spotaproject.firebaseapp.com",
+    databaseURL: "https://spotaproject.firebaseio.com",
+    projectId: "spotaproject",
+    storageBucket: "spotaproject.appspot.com",
+    messagingSenderId: "746405070068"
   },
   firebase_providers: [
     'google.com',
