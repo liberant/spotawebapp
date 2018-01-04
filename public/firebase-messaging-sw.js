fda0f078463d26fe3e5308bad3d4b9b1c1148c79
Noven Purnell-Webb <noven@liberant.com.au>
2018-01-05 06:27:21 +1000

    messaging update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
-debugger;
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 firebase.initializeApp({
   'messagingSenderId': '738533213905'
 });
@@ -11,7 +10,7 @@ const messaging = firebase.messaging();
 messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = 'Message Title';
   const notificationOptions = {
     body: 'Background Message body.',
     icon: '/apple-touch-icon.png'
